0e677c1b422859c2edc8b59f00c8ad5a4205a1a8
Tae Hee Lee <liver1379@naver.com>
2024-05-29 17:03:42 +0900

    1000개 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+new Array(1000).fill(0).map(e => 0)
+
 function Counter() {
   const 리턴값 = useState(0);
   const count = 리턴값[0];
@@ -12,7 +14,6 @@ function Counter() {
   if (count !== 0) {
     초기화버튼 = <button onClick={() => setCount(0)}>초기화</button>;
   }
-
   return (
     <div>
       <h1>{count}</h1>
@@ -28,13 +29,11 @@ function Counter() {
 }
 
 function App() {
-  return (
-    <div>
-      <Counter />
-      <Counter></Counter>
-      <Counter></Counter>
-    </div>
-  );
-}
+  const counterArray = [];
+  for (let i = 0; i < 1000; i++) {
+    counterArray.push(<Counter></Counter>);
+  }
+  return <div>{counterArray}</div>
+  }
 
 export default App;
