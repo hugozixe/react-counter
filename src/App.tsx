9386dac85eaf68ab66a15e94806acbdde2dbda03
Tae Hee Lee <liver1379@naver.com>
2024-05-29 16:06:16 +0900

    초기화 버튼 감춤 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ function App() {
   const 리턴값 = useState(0);
   const count = 리턴값[0];
   const setCount = 리턴값[1];
+
+  let 초기화버튼;
+  if (count !== 0) {
+    초기화버튼 = <button onClick={() => setCount(0)}>초기화</button>;
+  }
+
   return (
     <div>
       <h1>{count}</h1>
@@ -15,9 +21,8 @@ function App() {
       </button>
       <button onClick={() => setCount((현재카운트) => 현재카운트 - 1)}>
         감소
-      </button><button onClick={() => setCount(0)}>
-        초기화
       </button>
+      <button>{초기화버튼}</button>
     </div>
   );
 }
